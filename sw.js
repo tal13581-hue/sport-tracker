c6f9b723666692749b4b710a89600cbe39ac867c
tal13 <tal13581@gmail.com>
2026-08-19 11:11:41 +0300

    Full-circle tile art with done/not-done colors; aerobic exercise (50+level pts); today line in log; overall daily averages; per-day points breakdown; collapsible history; edit mode replaces wipe

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sportapp-v8";
+const CACHE = "sportapp-v9";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
