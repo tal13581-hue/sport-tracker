a003d2f21569bb1c9d1910c45cd325a7bb3a6f0a
tal13 <tal13581@gmail.com>
2026-08-23 15:51:05 +0300

    Fix clipped rightmost label in points chart; add day/week toggle and average to points chart

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sportapp-v11";
+const CACHE = "sportapp-v12";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
