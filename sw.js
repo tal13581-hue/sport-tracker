ddc0445cd82d671891b5e733680421d09f169dad
tal13 <tal13581@gmail.com>
2026-08-28 14:20:50 +0300

    Per-exercise chart: replace flat average line with moving-average trend line too

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sportapp-v14";
+const CACHE = "sportapp-v15";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
