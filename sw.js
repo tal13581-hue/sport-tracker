4328840fe4d97c09bfbf5634ab497cbc81116d8b
tal13 <tal13581@gmail.com>
2026-08-29 15:55:09 +0300

    Add longest-streak record (overall and per exercise) alongside current streak

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sportapp-v16";
+const CACHE = "sportapp-v17";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
